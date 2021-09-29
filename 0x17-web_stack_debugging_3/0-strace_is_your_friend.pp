77ea7cc537e15b064e7245675bf2d4d05594d702
stevengm45 <stevengm45@gmail.com>
2021-09-29 17:46:01 -0500

    web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# web stack debugging 3
+exec { 'fix':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}
